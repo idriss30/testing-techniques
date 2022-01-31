bd33b41027283057393bda15aeacd1b09c294aff
idriss30 <idriss.cissoko@hotmail.com>
2022-01-31 01:36:03 -0500

    testing tracking of inventory by using stub

--- end2end/inventory.test.js
+++ end2end/inventory.test.js
@@ -1,6 +1,10 @@
 const { inventory, addToInventory, getInventory } = require("./inventory");
+const logger = require("./logger");
 
 beforeEach(() => inventory.clear());
+beforeAll(() => jest.spyOn(logger, "logInfo").mockImplementation(jest.fn())); // turn spy intp stub by passing dummy function to avoid polluting the console
+beforeAll(() => jest.spyOn(logger, "logError").mockImplementation(jest.fn()));
+afterEach(() => logger.logInfo.mockClear()); //The spy’s mockClear function will reset both the spy.mock.calls and spy.mock.instances arrays
 
 test("cancel operation for invalid quantities", () => {
   //expect.hasAssertions(); // cause text to fail if it doesn't execute at least one assertions
@@ -43,4 +47,47 @@ test("generated at in the past", () => {
   const result = getInventory();
   const currentTime = new Date(Date.now() + 1); // add one milisecond to the current date
   expect(result.generatedAt).toBeBefore(currentTime);
+});
+
+// implementing a spy to see if logger receives the right parameters
+
+test("login items", () => {
+  addToInventory("cheesecake", 2);
+  // console.log(logger.logInfo.mock);
+
+  //expect logger to be call once for example
+  expect(logger.logInfo.mock.calls).toHaveLength(1);
+  //get the object from the mock.calls method
+
+  const loggerArguments = logger.logInfo.mock.calls[0];
+  const [firstArg, lastArg] = loggerArguments;
+
+  // asserting on usage of spy only after exercising it
+
+  expect(firstArg).toEqual({ item: "cheesecake", n: 2 });
+  expect(lastArg).toEqual("item added to the inventory");
+});
+
+test("get inventory items", () => {
+  inventory.set("croissants", 2);
+  getInventory();
+  // call logger once
+  expect(logger.logInfo.mock.calls).toHaveLength(1);
+
+  const inventoryArgs = logger.logInfo.mock.calls[0];
+  const [firstArg, lastArg] = inventoryArgs;
+
+  expect(firstArg).toEqual({ content: { croissants: 2 } });
+  expect(lastArg).toEqual("inventory items fetched");
+});
+
+// checking if logError is added with add to Inventory
+test("logError when adding not a number to inventory", () => {
+  inventory.set("cheesecake", "not a number");
+
+  expect(logger.logError.mock.calls).toHaveLength(1);
+  const expectedResponse = logger.logError.mock.calls[0];
+  const [first, last] = expectedResponse;
+  expect(first).toEqual({ n: "not a number" });
+  expect(last).toEqual("could not be added because it is not a number");
 });
